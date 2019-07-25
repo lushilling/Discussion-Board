4abd0f6ada79227013eaa85f5f84e91307aec254
lushilling <lshilling@hotmail.co.uk>
2019-07-25 11:01:24 +0100

    Functioning CRUD with mongoose

--- routes/name.js
+++ routes/name.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const _=require("lodash");
-const user = require("../models/schema");
+const User = require("../models/schema");
 
 // @route   GET item/test
 // @desc    Tests route
 // @access  Public
 router.get("/test", (req, res) => {
     res.json({
-      message: "Username"
+      message: "User"
     });
   });
 
@@ -17,7 +17,7 @@ router.get("/test", (req, res) => {
 //@access Public
 router.get("/all", (req, res) => {
     const errors = {};
-    user.find()
+    User.find()
     .then(users => {
         if (!users){
             errors.onUsers = "There are no users";
@@ -33,54 +33,54 @@ router.get("/all", (req, res) => {
 // @access  Public
 router.get("/username", (req, res) => {
     const errors = {};
-    user.find({'username': req.body.username})
-      .then(user => {
-        if (!user) {
-          errors.nouser = "There are no users";
+    User.find({'username': req.body.username})
+      .then(users => {
+        if (!users) {
+          errors.noUsers = "There are no users";
           res.status(404).json(errors);
         }
-        res.json(user);
+        res.json(users);
       })
-      .catch(err => res.status(404).json({ nouser: "There are no users" }));
+      .catch(err => res.status(404).json({ noUsers: "There are no users" }));
   });
   
 // @route   DELETE name/deleteUsername
 // @desc    Delete items from one username
 // @access  Public
 router.delete("/deleteUsername", (req, res) => {
-    user.deleteOne({'username': req.body.username})
+    User.deleteOne({'username': req.body.username})
     .then(({ok, n}) => {
-        res.json({ nouser: "Deleted :)" });
+        res.json({ noUsers: "Deleted :)" });
     })
-      .catch(err => res.status(404).json({ nouser: "Can not delete user" }));
+      .catch(err => res.status(404).json({ noUsers: "Can not delete user" }));
   });
 
 // @route   POST name/addUser
 // @desc    Add user
 // @access  Public
 router.post("/addUser", (req, res) =>{
-    const newUser = new user({
+    const user = new User({
         username: req.body.username,
         content: req.body.content
     });
-    newUser.save()
+    user.save()
     .then(()=> {
-        res.json(newUser);
+        res.json(user);
          console.log('complete')
     })
-    .catch(err => res.status(404).json({ newUser: "User couldn't be added" }));
+    .catch(err => res.status(404).json({ noUsers: "User couldn't be added" }));
 });
 
 // @route   PUT name/updateUsername
 // @desc    Update items from one username
 // @access  Public
 router.put("/updateUser", (req, res) => {
-    user.replaceOne({'username': req.body.username},
-    {'username': req.body.upUsername, 'content': req.body.upContent})
+    User.replaceOne({'username': req.body.username},
+    {'username': req.body.username, 'content': req.body.content})
     .then(({ok, n}) => {
-        res.json({ nouser: "updated :)" });
+        res.json({ noUsers: "updated :)" });
     })
-    .catch(err => res.status(404).json({ nouser: "User can not be updated" }));
+    .catch(err => res.status(404).json({ noUsers: "User can not be updated" }));
 });
 
 
