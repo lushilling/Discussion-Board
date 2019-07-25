bf6694ffc8907e0d9f3d8cc80bf64118d2bfe739
lushilling <lshilling@hotmail.co.uk>
2019-07-25 11:41:01 +0100

    validation functions

--- validator/is-empty.js
+++ validator/is-empty.js
@@ -1,5 +1,3 @@
-const Validator = require("validator");
-const isEmpty = require("./is-empty");
 
 const isEmpty = value =>
 value === undefined ||
--- validator/validator.js
+++ validator/validator.js
@@ -1,24 +1,24 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-module.exports = function validateLoginInput(data) {
+module.exports = function validateLoginInput(user) {
     let errors = {};
 
-    // if these values are not present in the data object we are validating then they will be set to empty strings for the Validator.isEmpty
-    data.username = !isEmpty(data.username) ? data.username : "";
-    data.content = !isEmpty(data.content) ? data.content : "";
+    // if these values are not present in the user object we are validating then they will be set to empty strings for the Validator.isEmpty
+    user.username = !isEmpty(user.username) ? user.username : "";
+    user.content = !isEmpty(user.content) ? user.content : "";
 
     //username validation rules
-    if (Validator.isEmpty(data.username)) {
+    if (Validator.isEmpty(user.username)) {
         errors.username = "Username field is required";
     }
 
-    if (!Validator.isUsername(data.username)) {
+    if (!Validator.isUsername(user.username)) {
         errors.username = "Username is invalid";
     }
 
     //content validation rules
-    if (!Validator.isContent(data.content)) {
+    if (!Validator.isContent(user.content)) {
         errors.content = "Content is invalid";
     }
 
